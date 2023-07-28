f11054196cdcda80c89a6a2150ec93202d30336c
“yoshiringo” <hey_kikokiko@hotmail.co.jp>
2023-07-28 17:59:19 +0900

    “タグ機能”

--- toys/forms.py
+++ toys/forms.py
@@ -5,6 +5,8 @@ class DateInput(forms.DateInput):
     input_type = 'date'
 
 class ToysModelForm(forms.ModelForm):
+    tags = forms.CharField(required=False)
+
     class Meta:
         model = Toys
         fields = "__all__"
--- toys/models.py
+++ toys/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 class Toys(models.Model):
     name = models.CharField(max_length=32, verbose_name='おもちゃ名')
     price = models.BigIntegerField(verbose_name='金額', null=True, blank=True)
@@ -9,11 +8,7 @@ class Toys(models.Model):
         ('ほぼ使わない', 'ほぼ使わない'),
         ('たまに', 'たまに'),
         ('いつも', 'いつも'),
-        )
+    )
     freq = models.CharField(verbose_name='頻度', max_length=50, choices=FREQ, blank=True)
     image = models.ImageField(upload_to='media_local', blank=True, null=True)
-    tags = models.CharField(verbose_name='タグ', max_length=32, blank=True, null=True)
-
-    class Meta:
-        db_table = 'blogs'
-
+    tags = models.CharField(verbose_name='頻度', blank=True, null=True)
